d0234f9230d8e1b68995f022e031967774e0e6f9
priyanshupandey29 <priyanshupandey299@gmail.com>
2021-07-19 13:47:41 +0530

    Completed

---
+++ Day 14/script.js
@@ -0,0 +1,9 @@
+function pageloaded() {
+  alert("Page is loaded");
+}
+function resizewindow() {
+  alert("User trying to resize the window");
+}
+function onvideoplay() {
+  console.log("video can be seen now");
+}
